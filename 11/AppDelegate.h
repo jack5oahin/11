ba8d6e0bd2d9b733c3740abb4388b310a0df16be
jack4oahin <jack4oahin@gmail.com>
2018-07-03 22:59:29 +0800

    Initial Commit

---
+++ 11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  11
+//
+//  Created by 王鑫 on 2018/7/3.
+//  Copyright © 2018年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
